9a890a929a3e4ce3ec771fefe0a5523e572e163e
Travis Bayer <travis.bayer@selu.edu>
2022-09-27 20:20:22 +0000

    Revert "new function created for NA removals"
    This reverts commit 9e5a588158e1d40aa967f344ce10e7369bbff4ad.

--- scripts/delete_NAs.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-data_push_example <_ read_csv("../data/Butterfly_data.csv")
-
-Delete_NAs <- function(dataset = data){
-  new_data <- data %>% 
-    na.omit()
-  return(new_data)
-}
